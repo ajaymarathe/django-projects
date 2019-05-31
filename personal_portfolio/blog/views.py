406062292ba1bc34dcd842ae2628d97d50fbd110
Ajay Marathe <ajay.marathe@xebecindia.com>
2019-05-31 18:01:26 +0530

    commiting personal portfolio project with blog

---
+++ personal_portfolio/blog/views.py
@@ -0,0 +1,47 @@
+# Create your views here.
+
+from django.shortcuts import render
+from blog.models import Post
+from blog.forms import CommentForm
+from blog.models import Post, Comment
+
+# index view
+def blog_index(request):
+    posts = Post.objects.all().order_by('-created_on')
+    context = {
+        "posts": posts,
+    }
+    return render(request, "blog_index.html", context)
+
+# category view
+def blog_category(request, category):
+    posts = Post.objects.filter(
+        categories__name__contains=category
+    ).order_by(
+        '-created_on'
+    )
+    context = {
+        "category": category,
+        "posts": posts
+    }
+    return render(request, "blog_category.html", context)
+
+# blog detail
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post
+            )
+            comment.save()
+
+    comments = Comment.objects.filter(post=post)
+    context = {"post": post, "comments": comments, "form": form}
+
+    return render(request, "blog_detail.html", context)
---
+++ personal_portfolio/projects/views.py
@@ -0,0 +1,20 @@
+# Create your views here.
+
+from django.shortcuts import render
+from projects.models import Project
+
+#show all portfolio page.
+def project_index(request):
+    projects = Project.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render(request, 'project_index.html', context)
+
+# show detail portfolio
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
